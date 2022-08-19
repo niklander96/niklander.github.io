857bb9cb471d7d139c766e4b2545c760905fccdf
Никита <nik.loktev151069@gmail.com>
2022-08-19 17:39:47 +0300

    Updates

---
+++ js/6.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{3:function(e,i){var n=document.querySelector(".swiper");if(document.documentElement.clientWidth<="768")new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0},slideToClickedSlide:!0,slidesPerView:"auto"});else n.classList.remove("swiper")}}]);
+//# sourceMappingURL=6.bundle.js.map
